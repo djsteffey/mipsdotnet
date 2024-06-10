3f3dd3c6c8439937c35f02fb798c6a15b5e31c7a
Dan Steffey <djsteffey@gmail.com>
2024-06-10 08:24:04 -0400

    none

--- src/System.cs
+++ src/System.cs
@@ -1,12 +1,14 @@
 namespace mipsdotnet{
     class System{
         // variables
+        private bool m_running;
         private CPU m_cpu;
         private MMU m_mmu;
         
 
         // methods
         public System(){
+            this.m_running = false;
             this.m_cpu = new CPU();
             this.m_mmu = new MMU();
         }
@@ -16,15 +18,35 @@ namespace mipsdotnet{
         }
 
         public void run(){
-            
+            // flag as running
+            this.m_running = true;
+
+            // keep on stepping
+            while (this.m_running){
+                this.step();
+            }
+        }
+
+        public void reset(UInt32 pc){
+            // reset the cpu
+            this.m_cpu.reset(pc);
         }
 
         public void halt(){
-
+            // flag as stop running
+            this.m_running = false;
         }
 
-        public void reset(){
+        public void resume(){
+            // start running again
+            this.run();
+        }
 
+        public void step(){
+            // fetch instruction from pc address in mmu
+            UInt32 instruction = this.m_mmu.readUint32(this.m_cpu.Registers.PC);
+
+            // decode and execute instruction
         }
     }
 }
